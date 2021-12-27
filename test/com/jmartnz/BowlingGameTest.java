055294155449093e4817f9c08fca7d45c3d6c4ff
Josue Martinez Fernandez <jmartnz.designer@gmail.com>
2021-12-27 20:17:29 +0100

    test all ones

--- test/com/jmartnz/BowlingGameTest.java
+++ test/com/jmartnz/BowlingGameTest.java
@@ -14,4 +14,13 @@ class BowlingGameTest {
         }
         assertEquals(0, game.score());
     }
+
+    @Test
+    void testAllOnes() {
+        Game game = new Game();
+        for (int i = 0; i < 20; i++) {
+            game.roll(1);
+        }
+        assertEquals(20, game.score());
+    }
 }
