a55d6a17e82811630aa79cd47e76dd872185e9e0
Josue Martinez Fernandez <jmartnz.designer@gmail.com>
2021-12-27 20:15:22 +0100

    test gutter game

---
+++ src/com/jmartnz/Game.java
@@ -0,0 +1,11 @@
+package com.jmartnz;
+
+public class Game {
+    public void roll(int pins) {
+
+    }
+
+    public int score() {
+        return 0;
+    }
+}
--- test/com/jmartnz/BowlingGameTest.java
+++ test/com/jmartnz/BowlingGameTest.java
@@ -1,6 +1,17 @@
 package com.jmartnz;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BowlingGameTest {
+
+    @Test
+    void testGutterGame() {
+        Game game = new Game();
+        for (int i = 0; i < 20; i++) {
+            game.roll(0);
+        }
+        assertEquals(0, game.score());
+    }
 }
