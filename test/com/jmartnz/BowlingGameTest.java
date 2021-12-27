2f7e2ea81dce06fefd1ffce0495da6a212253210
Josue Martinez Fernandez <jmartnz.designer@gmail.com>
2021-12-27 20:30:02 +0100

    little refactor

--- test/com/jmartnz/BowlingGameTest.java
+++ test/com/jmartnz/BowlingGameTest.java
@@ -1,26 +1,32 @@
 package com.jmartnz;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BowlingGameTest {
 
+    private Game game;
+
+    @BeforeEach
+    void setUp() {
+        game = new Game();
+    }
+
     @Test
     void testGutterGame() {
-        Game game = new Game();
-        for (int i = 0; i < 20; i++) {
-            game.roll(0);
-        }
+        rollMany(20, 0);
         assertEquals(0, game.score());
+    }
+
+    private void rollMany(int n, int pins) {
+        for (int i = 0; i < n; i++) game.roll(pins);
     }
 
     @Test
     void testAllOnes() {
-        Game game = new Game();
-        for (int i = 0; i < 20; i++) {
-            game.roll(1);
-        }
+        rollMany(20, 1);
         assertEquals(20, game.score());
     }
 }
